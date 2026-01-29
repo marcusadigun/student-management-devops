4ccabbf31027bc3e55d6071a8dbe86818b37066b
marcus <adigunmarcus@gmail.com>
2026-01-29 16:26:57 +0100

    Finalized IaC with outputs and validated resources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "app_url" {
-  value       = "http://${aws_instance.hms_server.public_ip}"
+  value       = "http://${aws_instance.hmsapp_server.public_ip}"
   description = "The public URL of the Student Management System"
 }
 
 output "ssh_command" {
-  value       = "ssh -i your-key.pem ubuntu@${aws_instance.hms_server.public_ip}"
+  value       = "ssh -i your-key.pem ubuntu@${aws_instance.hmsapp_server.public_ip}"
   description = "Command to SSH into the instance if you need to debug"
 }
